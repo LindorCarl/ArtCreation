1290c28bc738fda21f6069a6d8bf8a1d24a0016e
LindorCarl <lindorcarl8@gmail.com>
2023-08-20 20:14:15 +0200

    Add fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,19 +3,21 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Entity\Blogpost;
+use App\Entity\Categorie;
+use App\Entity\Peinture;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    //private $encoder;
 
     // Pour éviter des erreurs, "UserPasswordEncoderInterface" est remplacé par "UserPasswordHasher"
-    private $encoder;
+    private UserPasswordHasherInterface $encoder;
 
-    public function __construct(UserPasswordEncoderInterface $encoder)
+    public function __construct(UserPasswordHasherInterface $encoder)
     {
         $this->encoder = $encoder;
     }
@@ -35,10 +37,59 @@ class AppFixtures extends Fixture
             ->setAPropos($faker->text())
             ->setInstagram('instagram');
         
-        $password = $this->encoder->encodePassword($user, 'password');
+        $password = $this->encoder->hashPassword($user, 'password');
         $user->setPassword($password);
 
         $manager->persist($user);
+
+        // Création de 10 Blogpost.
+        for ($i = 0; $i < 10; $i++) {
+            $blogpost = new Blogpost();
+
+            $blogpost->setTitre($faker->words(3, true))
+                ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTime))
+                ->setContenu($faker->text(350))
+                ->setSlug($faker->slug(3))
+                // Pour l'id de l'utilisateur.
+                ->setUser($user);
+
+            $manager->persist($blogpost);
+        }
+
+        // Les peintures sont classées par categorie.
+        for ($j = 0; $j < 5; ++$j) {
+            $categorie = new Categorie();
+    
+                $categorie->setNom($faker->word())
+                    ->setDescription($faker->words(10, true))
+                    ->setSlug($faker->slug());
+    
+                $manager->persist($categorie);
+            
+            // Création de 2 peintures/categorie.
+            for ($k = 0; $k < 2; $k++) {
+                
+                $peinture = new Peinture();
+
+                $peinture->setNom($faker->words(3, true))
+                    ->setLargeur($faker->randomFloat(2, 20, 60))
+                    ->setHauteur($faker->randomFloat(2, 20, 60))
+                    ->setEnVente($faker->randomElement([true, false]))
+                    ->setDateRealisation(\DateTimeImmutable::createFromMutable($faker->dateTime))
+                    ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTime))
+                    ->setDescription($faker->text())
+                    ->setPortfolio($faker->randomElement([true, false]))
+                    ->setSlug($faker->slug())
+                    ->setFile('https://picsum.photos/200/300')
+                    ->addCategorie($categorie)
+                    ->setPrix($faker->randomFloat(2, 100, 9999))
+                    ->setUser($user);
+                
+                $manager->persist($peinture);
+    
+            }
+        }
+
         // Pour insérer les données en BDD.
         $manager->flush();
     }
